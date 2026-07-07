fa84f9cab4bcaa9ef920c6c722d2d1080d8c0a10
feryts <81550388+feryts@users.noreply.github.com>
2026-07-07 18:02:22 +0300

    Create wallet.js

---
+++ js/wallet.js
@@ -0,0 +1,21 @@
+const coinTransfer=document.getElementById("coinTransfer");
+const withdraw=document.getElementById("withdraw");
+const history=document.getElementById("history");
+
+coinTransfer.onclick=()=>{
+
+alert("Coin Transfer Sistemi v2.0 ile aktif olacak.");
+
+}
+
+withdraw.onclick=()=>{
+
+alert("Çekim Sistemi hazırlanıyor.");
+
+}
+
+history.onclick=()=>{
+
+alert("Detaylı geçmiş yakında.");
+
+}
